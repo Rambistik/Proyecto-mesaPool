dce71c3d3b4e709895a4c87e27243cfc69b8fa01
rcadiz2020 <rcadiz2020@udec.cl>
2022-11-24 23:51:11 -0300

    bola actualizada aun no funcional

--- MesaDePool/src/mesadepool/Bola.java
+++ MesaDePool/src/mesadepool/Bola.java
@@ -1,8 +1,16 @@
 package mesadepool;
 
+import java.awt.Rectangle;
+
 
 public class Bola {
     public bola(){
-        
+
+    }
+    private void generarbola(){
+    Rectangle bola = new Rectangle();
+    bola.width = 32;
+    bola.height = 32;
+    bola.add(bola);
     }
 }
