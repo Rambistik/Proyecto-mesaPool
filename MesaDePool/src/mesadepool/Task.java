4e6293c4d102f9bfede72685ae946ef25de6c06b
rcadiz2020 <rcadiz2020@udec.cl>
2022-12-11 03:48:02 -0300

    Añadida clase task

--- MesaDePool/src/mesadepool/Task.java
+++ MesaDePool/src/mesadepool/Task.java
@@ -1,6 +1,18 @@
-
 package mesadepool;
 
-class Task {
-    
+import java.awt.Graphics;
+import java.util.TimerTask;
+
+public class Task extends TimerTask {
+
+    MesaDePool parent;
+    Graphics g;
+
+    public Task(MesaDePool myParent) {
+        parent = myParent;
+    }
+
+    public void run() {
+        parent.update();
+    }
 }
