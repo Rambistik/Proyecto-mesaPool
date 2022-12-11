360c63f74ce139e38207db92cc84ce539684e2cc
rcadiz2020 <rcadiz2020@udec.cl>
2022-12-11 15:48:55 -0300

    Nuevo color de pelotas

--- MesaDePool/src/mesadepool/Bola.java
+++ MesaDePool/src/mesadepool/Bola.java
@@ -42,37 +42,8 @@ public class Bola{
     }
     
     public void paint(Graphics g){
-        g.setColor(Color.white);
-
-        g.setColor(Color.black);
-           
-        g.setColor(Color.red);
-        
-        g.setColor(Color.red);
-        
-        g.setColor(Color.green);
-        
-        g.setColor(Color.green);
-        
-        g.setColor(Color.blue);
-        
-        g.setColor(Color.blue);
-        
-        g.setColor(Color.yellow);
-        
-        g.setColor(Color.yellow);
-        
-        g.setColor(new Color(102,0,153));
-        
-        g.setColor(new Color(102,0,153));
-        
-        g.setColor(new Color(255,102,0));
-        
-        g.setColor(new Color(255,102,0));
-        
-        g.setColor(new Color(102,52,0));
-        
-        g.setColor(new Color(102,52,0));
+ 
+        g.setColor(new Color(255,128,0));
         
         int diametro = (int) (radio * 2);
         
